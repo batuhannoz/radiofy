4bec6791f96697369fc78ccb6448419d28a268cf
batuhannoz <b.ozdmr23@gmail.com>
2022-02-17 12:20:45 +0300

    add custom dark theme scrollbar

--- frontend/src/pages/Clubs.vue
+++ frontend/src/pages/Clubs.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-white flex flex-wrap overflow-y-auto">
+    <div class="text-white flex flex-wrap overflow-y-auto scroller">
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-purple-700"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-SpotifyGreen"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-blue-600"></div>
@@ -26,6 +26,18 @@
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-green-600"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-pink-600"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-purple-700"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-SpotifyGreen"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-blue-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-green-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-pink-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-SpotifyGreen"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-blue-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-green-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-pink-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-SpotifyGreen"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-blue-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-green-600"></div>
+      <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-pink-600"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-SpotifyGreen"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-blue-600"></div>
       <div class="flex-initial m-auto my-6 aspect-square h-40 w-40 bg-green-600"></div>

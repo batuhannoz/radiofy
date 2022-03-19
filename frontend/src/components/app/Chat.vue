53e934db339b8c9a74c5d0f4d339d6f856efdcab
bozdm <b.ozdmr23@gmail.com>
2022-03-19 15:11:53 +0300

    chat split into components

--- frontend/src/components/app/Chat.vue
+++ frontend/src/components/app/Chat.vue
@@ -23,53 +23,11 @@
     <div class="flex flex-col w-full" style="height: calc(100% - 48px)">
       <div v-bind:class="{[currentChatColor]: true}" class="w-full h-10 mt-2 font-semibold pt-1.5 text-center rounded-t-xl">{{chatHeader}}</div>
       <div class="relative bg-[#1E1E1E] mt-1.5 mx-1.5 mb-4 rounded-xl" style="height: calc(100% - 32px)">
-        <div class="m-2 flex flex-row">
-          <div class="text-SpotifyGreen">
-            Harlan Loflin:
-          </div>
-          <div>
-             hi!
-          </div>
-        </div>
-        <div class="m-2 flex flex-row">
-          <div class="text-purple-700">
-            Jonathan Weddel:
-          </div>
-          <div>
-            hello
-          </div>
-        </div>
-        <div class="m-2 flex flex-row">
-          <div class="text-blue-700">
-            Marcell Brodrick:
-          </div>
-          <div>
-            hello
-          </div>
-        </div>
-        <div class="m-2">
-          <div class="text-pink-700">
-            Erasmo Piccirillo:
-          </div>
-          Anyone want to suggest a song?
-        </div>
-        <div class="m-2">
-          <div class="text-yellow-700">
-            Marcell Brodrick:
-          </div>
-          I'm listening to NF these days, check it out if you want.
-        </div>
-        <div class="m-2">
-          <div class="text-purple-700">
-            Jonathan Weddel:
-          </div>
-          if you want i can open NF club so we can listen together.
-        </div>
-        <div class="flex flex-row inset-x-0 bottom-0 absolute">
-          <input class="shadow appearance-none border rounded-l-xl w-full py-2 px-3 text-gray-700 leading-tight" type="text" placeholder="Write Message">
-          <button v-bind:class="{[currentChatColor]: true}" class="inline-block px-3 py-2 text-white font-medium text-xs leading-tight uppercase rounded-r-xl shadow-md hover:opacity-75 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:opacity-50 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
-            <send-icon/>
-          </button>
+        <div style="height: calc(100% - 50px)">
+          <!--chat type check-->
+          <club-chat v-if="currentChat === 'club'"/>
+          <friends-chat v-else-if="currentChat === 'friend'"/>
+          <global-chat v-else/>
         </div>
       </div>
     </div>
@@ -80,13 +38,17 @@
 import WorldIcon from "@/assets/icon/World.vue";
 import FriendsIcon from "@/assets/icon/Friends.vue";
 import ChatIcon from "@/assets/icon/Chat.vue";
-import SendIcon from  "@/assets/icon/Send.vue";
+import ClubChat from "../chat/Club.vue"
+import GlobalChat from "../chat/Global.vue"
+import FriendsChat from "../chat/Friends.vue"
 export default {
   components: {
     WorldIcon,
     FriendsIcon,
     ChatIcon,
-    SendIcon
+    ClubChat,
+    GlobalChat,
+    FriendsChat
   },
   data() {
     return{
@@ -97,23 +59,23 @@ export default {
   },
   methods: {
     SetGlobal() {
+      console.log(this.currentChat)
       this.chatHeader = "Global Chat"
       this.currentChatColor = "bg-SpotifyGreen"
       this.currentChat = "global"
     },
     SetFriends() {
+      console.log(this.currentChat)
       this.chatHeader = "Friends"
       this.currentChatColor = "bg-purple-700"
       this.currentChat = "friend"
     },
     SetClub() {
+      console.log(this.currentChat)
       this.chatHeader = "Club Chat"
       this.currentChatColor = "bg-[#545454]"
       this.currentChat = "club"
     },
-    SendMessage() {
-
-    }
   }
 }
 
---
+++ frontend/src/components/chat/Global.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-SpotifyGreen h-full">
+    {{ReceviedMessage}}
+    <div class="flex flex-row inset-x-0 bottom-0 absolute">
+      <input v-model="Message" class="shadow appearance-none border rounded-l-xl w-full py-2 px-3 text-gray-700 leading-tight" type="text" placeholder="Write Message">
+      <button @click="SendMessage" class="bg-SpotifyGreen inline-block px-3 py-2 text-white font-medium text-xs leading-tight uppercase rounded-r-xl shadow-md hover:opacity-75 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:opacity-50 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="button" id="button-addon2">
+        <send-icon/>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import SendIcon from  "@/assets/icon/Send.vue";
+export default {
+  components:{
+    SendIcon
+  },
+  data(){
+    return{
+      Message: "",
+      ReceviedMessage: ""
+    }
+  },
+  methods: {
+    SendMessage() {
+      this.ReceviedMessage += this.Message
+      this.Message = ""
+    }
+  }
+}
+</script>
